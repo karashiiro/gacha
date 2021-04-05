6d7868344aa0cf18c9ea8ad0dcef8f589066956f
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-04-05 14:55:01 -0700

    Set up logging

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"os"
+
+	"go.uber.org/zap"
+)
+
+func NewLogger() (*zap.Logger, error) {
+	cfg := zap.NewProductionConfig()
+	cfg.OutputPaths = append(cfg.OutputPaths, "log/gacha.log")
+
+	// Create log directory if it does not exist
+	_, err := os.Stat("log/")
+	if os.IsNotExist(err) {
+		os.Mkdir("log/", 0644)
+	}
+
+	return cfg.Build()
+}
+
 func main() {
-	//
+	logger, err := NewLogger()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+	sugar := logger.Sugar()
+
+	sugar.Infow("Application started.")
 }
