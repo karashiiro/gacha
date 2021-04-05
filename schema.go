371fd496cc757bcb435a959fb0c12d9ebb704a2b
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-04-05 16:10:35 -0700

    Make DB columns lowercase

--- schema.go
+++ schema.go
@@ -2,6 +2,6 @@ package main
 
 // Drop represents a single possible roll from the gacha.
 type Drop struct {
-	ID   uint32
-	Rate float32
+	ID   uint32  `gorm:"column:id"`
+	Rate float32 `gorm:"column:rate"`
 }
