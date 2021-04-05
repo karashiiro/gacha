7bb423545a449f52b1f65bbab79053bad1b5dabe
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-04-05 15:37:58 -0700

    Move logger initialization to own file

--- main.go
+++ main.go
@@ -1,25 +1,5 @@
 package main
 
-import (
-	"os"
-
-	"go.uber.org/zap"
-)
-
-// NewLogger creates a file/console logger.
-func NewLogger() (*zap.Logger, error) {
-	cfg := zap.NewProductionConfig()
-	cfg.OutputPaths = append(cfg.OutputPaths, "log/gacha.log")
-
-	// Create log directory if it does not exist
-	_, err := os.Stat("log/")
-	if os.IsNotExist(err) {
-		os.Mkdir("log/", 0644)
-	}
-
-	return cfg.Build()
-}
-
 func main() {
 	// Set up logging
 	logger, err := NewLogger()
